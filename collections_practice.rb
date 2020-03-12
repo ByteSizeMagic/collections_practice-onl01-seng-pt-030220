e65906707f42704b1d5f6ae18a9e590c279b2aa6
Crystal Schooling <bytesizemagic@gmail.com>
2020-03-12 09:10:24 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -46,7 +46,19 @@ end
 
 
 def kesha_maker(array)
+  keshafied = []
+  array.each do |character|
+    keshafied << 
+  end
+  keshafied
+end
 
+def hamburger(toppings)
+    my_statements = []
+    toppings.each do |topping|
+        my_statements << "I love #{topping} on my burgers!"
+    end
+    my_statements
 end
 
 
