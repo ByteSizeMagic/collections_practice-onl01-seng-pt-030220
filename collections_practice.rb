fe19b7a27d1af2c61b6f92829f6022f28e9cd181
Crystal Schooling <bytesizemagic@gmail.com>
2020-03-12 08:30:25 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,54 @@
+# Build a method sort_array_asc that takes in an array of integers and
+# returns a copy of the array with the integers in ascending order.
+def sort_array_asc(integers)
+    integers.sort do |a, b|
+        a <=> b
+    end
+end
 
+
+def sort_array_desc(integers)
+  integers.sort.reverse do |a, b|
+      a <=> b
+  end
+end
+
+
+def sort_array_char_count(strings)
+
+end
+
+
+def swap_elements(array)
+
+end
+
+
+def swap_elements_from_to(array, index, destination_index)
+
+end
+
+
+def reverse_array(array)
+
+end
+
+
+def kesha_maker(array)
+
+end
+
+
+def find_a(array)
+
+end
+
+
+def sum_array(array)
+
+end
+
+
+def add_s(array)
+
+end
